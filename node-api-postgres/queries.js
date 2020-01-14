f5324030855e66c946cf04fecf6bf4d2c7118e44
Szymon Kolber <szymon.kolber@alior.pl>
2020-01-14 19:31:15 +0100

    Ejs added

--- node-api-postgres/queries.js
+++ node-api-postgres/queries.js
@@ -7,11 +7,15 @@ const pool = new Pool({
   port: 5432,
 })
 
-const getUsers = (request, response) => {
-  pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
+const getPlays = (request, response) => {
+  pool.query('SELECT * FROM sztuka ORDER BY id ASC', (error, results) => {
     if (error) {
       throw error
     }
     response.status(200).json(results.rows)
   })
+}
+
+module.exports = {
+	getPlays
 }
