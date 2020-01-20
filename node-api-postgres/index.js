c3960ff83e647c119dfc9c241d53a7cf13120a55
natalia <natalia@inuits.io>
2020-01-20 20:11:09 +0100

    styles added

--- node-api-postgres/index.js
+++ node-api-postgres/index.js
@@ -12,7 +12,7 @@ app.use(
     extended: true
   })
 );
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/static"));
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
